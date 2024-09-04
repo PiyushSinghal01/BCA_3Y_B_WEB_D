95c407b160a5d584ba0edeca0ea13b42100b86a6
Piyush Singhal <singhalpiyush789@gmail.com>
2024-09-04 14:54:00 +0530

    for in and for of loop

---
+++ for_in__for_of_loop.js
@@ -0,0 +1,36 @@
+let obj = {
+    name : "Piyush",
+    age : 19,
+    course : "BCA"
+}
+
+let arr = [1,2,3,6,7,8];
+
+// // for in loop with obj   // return keys
+// for(let i in obj)
+// {
+//     // console.log(i);
+//     console.log(i , ":" , obj[i]);
+// }
+
+// // for in loop with array   // return index
+// for(let i in arr)
+// {
+//     console.log(i);
+// }
+
+
+
+
+
+// for of loop with obj    // return 
+for (let i of obj) 
+{
+    console.log(i);
+}
+
+// // for of loop with array  // return value
+// for (let i of arr) 
+// {
+//   console.log(i);
+// }
